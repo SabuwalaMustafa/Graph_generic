ba562e8e9c305ae6eea465a9a1932f9f9e2429f3
Harshil <harshil.sukhadia@gmail.com>
2014-09-21 00:20:09 +0530

    NODELIST class Added

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Graph<T> {
+	
+	HashMap<String, ArrayList<node>> adjacencyList;
+	
+	Graph(){
+		
+	}
+	
+	public boolean addNode(node N)
+	{
+	ArrayList<node> temp=new ArrayList<node>();
+	
+	adjacencyList.put(N.tag,temp);
+		
+	}
 
 }
--- src/node.java
+++ src/node.java
@@ -1,4 +1,6 @@
 
 public class node {
+	
+	String tag;
 
 }
